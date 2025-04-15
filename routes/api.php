ba98ff2df6078994f03582b02b352679308cfbc4
aarondesu <aaronneielaro@gmail.com>
2025-04-15 16:00:23 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,15 @@ use App\Http\Controllers\Auth\AdminAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::group(['prefix' => 'v1'], function () {
+
     Route::group(['prefix' => 'admin'], function () {
         Route::group(['prefix' => 'auth'], function () {
             Route::post('/login', [AdminAuthController::class, 'login']);
             Route::get('/logout', [AdminAuthController::class, 'logout'])->middleware(['auth:sanctum', 'ability:admin']);
+            Route::get('/user', function (Request $request) {
+                return $request->user();
+            })->middleware('auth:sanctum');
         });
     });
 });
