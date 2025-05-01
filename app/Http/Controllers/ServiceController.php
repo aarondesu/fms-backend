dd88df952c130112be15bf49430f908796fa787d
aarondesu <aaronneielaro@gmail.com>
2025-05-01 16:06:34 +0800

    feat: add services feature

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -1,10 +1,101 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Models\Service;
+use Exception;
 use Illuminate\Http\Request;
+use Validator;
 
 class ServiceController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except('retrieve');
+    }
+    public function index()
+    {
+        try {
+            $services = Service::all();
+
+            return response()->json(['success' => true, 'data' => $services]);
+        } catch (Exception $error) {
+            return $this->throwUnhandledErrorResponse();
+        }
+    }
+    public function create(Request $request)
+    {
+        try {
+            $validate = Validator::make($request->all(), [
+                'name'      => 'required',
+                'unit_type' => 'required',
+                'rate'      => 'required',
+            ]);
+
+            if (! $validate->fails()) {
+                $service            = new Service();
+                $service->name      = $request->name;
+                $service->unit_type = $request->unit_type;
+                $service->rate      = $request->rate;
+
+                $service->save();
+
+                return response()->json(['succes' => true, 'message' => 'OK']);
+            } else {
+                return response()->json(['success' => false, 'errors' => $validate->errors()], 400);
+            }
+
+        } catch (Exception $error) {
+            return $this->throwUnhandledErrorResponse();
+        }
+    }
+    public function retrieve($id)
+    {
+        try {
+            $service = Service::find($id);
+            if ($service && $service->exists()) {
+                return response()->json(['success' => true, 'data' => $service]);
+            } else {
+                return response()->json(['success' => false, 'errors' => ['Serivce does not exist.']], 400);
+            }
+
+        } catch (Exception $error) {
+            return $this->throwUnhandledErrorResponse();
+        }
+    }
+    public function update(Request $request, $id)
+    {
+        try {
+            $service = Service::find($id);
+            if ($service && $service->exists()) {
+                $service->name      = $request->name;
+                $service->unit_type = $request->unit_type;
+                $service->rate      = $request->rate;
+
+                $service->save();
+
+                return response()->json(['success' => true, 'message' => 'OK']);
+            } else {
+                return response()->json(['success' => false, 'errors' => ['Serivce does not exist.']], 400);
+            }
+
+        } catch (Exception $error) {
+            return $this->throwUnhandledErrorResponse();
+        }
+    }
+    public function delete(Request $request, $id)
+    {
+        try {
+            $service = Service::find($id);
+            if ($service && $service->exists()) {
+                $service->delete();
+
+                return response()->json(['success' => true, 'message' => 'OK']);
+            } else {
+                return response()->json(['success' => false, 'errors' => ['Serivce does not exist.']], 400);
+            }
+
+        } catch (Exception $error) {
+            return $this->throwUnhandledErrorResponse();
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Auth\AdminAuthController;
 use App\Http\Controllers\Auth\AdminPasswordController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,5 +31,14 @@ Route::group(['prefix' => 'v1'], function () {
         Route::get('/{id}', [AdminController::class, 'retrieve']);
         Route::put('/{id}', [AdminController::class, 'update']);
         Route::delete('/', [AdminController::class, 'delete']);
+    });
+
+    // SERVICES
+    Route::group(['prefix' => 'services'], function () {
+        Route::get('/', [ServiceController::class, 'index']);
+        Route::post('/', [ServiceController::class, 'create']);
+        Route::get('/{id}', [ServiceController::class, 'retrieve']);
+        Route::put('/{id}', [ServiceController::class, 'update']);
+        Route::delete('/{id}', [ServiceController::class, 'delete']);
     });
 });
